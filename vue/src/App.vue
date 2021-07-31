bd31853c4d3833e2c2655ff53174f7b092df5abf
zhoushing <2458474492@qq.com>
2021-07-31 18:52:19 +0800

    7-31 18:51 local

---
+++ vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div >
+    <!--头部-->
+    <Header/>
+  </div>
+  <!--主体-->
+  <div style="display: flex">
+    <!--侧边栏-->
+    <Aside />
+    <el-config-provider :locale="locale">
+    <!--内容区域-->
+    <Home />
+    </el-config-provider>
+  </div>
+  <router-view style="flex: 1"/>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Home from "@/views/Home";
+
+import {ElConfigProvider} from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+export default {
+  name: "Layout",
+  components: {
+    Home,
+    Header,
+    Aside,
+    [ElConfigProvider.name]:ElConfigProvider,//添加组件
+  },
+  data () {
+    return {
+      locale:zhCn,//给locale赋值
+    }
+  }
+}
+</script>
---
+++ vue/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+import '@/assets/css/global.css'
+
+createApp(App).use(store).use(router).use(ElementPlus, {size: 'small'}).mount('#app')
